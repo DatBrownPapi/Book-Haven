dee11c5f76ee1570d4e087fac26bf8b72f8ae809
DatBrownPapi <sandovaljaime89@gmail.com>
2026-03-31 02:15:01 -0600

    Create script.js
    handles requirements for all pages

---
+++ script.js
@@ -0,0 +1,24 @@
+// Function for Subscribe Button (Footer)
+function subscribeAlert() {
+    alert("Thank you for subscribing.");
+}
+
+// Function for Add to Cart Buttons (Gallery Page)
+function addToCartAlert() {
+    alert("Item added to the cart.");
+}
+
+// Function for Clear Cart Button (Gallery Page)
+function clearCartAlert() {
+    alert("Cart cleared.");
+}
+
+// Function for Process Order Button (Gallery Page)
+function processOrderAlert() {
+    alert("Thank you for your order.");
+}
+
+// Function for Contact Form Submit (About Us Page)
+function contactSubmitAlert() {
+    alert("Thank you for your message.");
+}
